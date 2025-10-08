53ab8222450cab45f92b153e132491ab2412ac7d
Stephen225 <128063929+Stephen225@users.noreply.github.com>
2025-10-08 18:14:16 -0400

    Add files via upload

--- shifter.py
+++ shifter.py
@@ -23,27 +23,31 @@ class Bug:
 		self.__shifter = Shifter()
 		self.bugs = [2**i for i in range(8)]
 		self.bugIndex = rand.randint(0,7)
-		self.go = True
 
 
 	def start(self):
-		try:
-			while self.go:
-				self.__shifter.shiftByte(self.bugs[self.bugIndex])
-				time.sleep(self.timestep)
-				if rand.randint(0,1) == 1:
-					self.bugIndex += 1
-					if self.bugIndex == 8: 
-						if not self.isWrapOn:
-							self.bugIndex = 6
-						else:
-							self.bugIndex = 0
-				else:
-					self.bugIndex -= 1
-					if self.bugIndex == -1: 
-						if not self.isWrapOn:
-							self.bugIndex = 2
-						else:
-							self.bugIndex = 8
-		except KeyboardInterrupt:
-			GPIO.cleanup()
+		self.go = True
+
+	def stop(self):
+		self.go = False
+
+	try:
+		while self.go:
+			self.__shifter.shiftByte(self.bugs[self.bugIndex])
+			time.sleep(self.timestep)
+			if rand.randint(0,1) == 1:
+				self.bugIndex += 1
+				if self.bugIndex == 8: 
+					if not self.isWrapOn:
+						self.bugIndex = 6
+					else:
+						self.bugIndex = 0
+			else:
+				self.bugIndex -= 1
+				if self.bugIndex == -1: 
+					if not self.isWrapOn:
+						self.bugIndex = 2
+					else:
+						self.bugIndex = 8
+	except KeyboardInterrupt:
+		GPIO.cleanup()
