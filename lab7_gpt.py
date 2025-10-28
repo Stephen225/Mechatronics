d953fdc74b43c239e777ee9b2c4273d38f64ca15
Stephen225 <128063929+Stephen225@users.noreply.github.com>
2025-10-28 14:37:09 -0400

    Add files via upload

--- lab7_gpt.py
+++ lab7_gpt.py
@@ -63,4 +63,69 @@ def make_page():
 		<span id="val2">{brightness[2]}</span>%
 	</div>
 
-	<scrip
+	<script>
+		function updateLED(led, val) {{
+			document.getElementById("val" + led).innerText = val;
+			fetch("/", {{
+				method: "POST",
+				headers: {{ "Content-Type": "application/x-www-form-urlencoded" }},
+				body: "led=" + led + "&level=" + val
+			}})
+			.then(res => res.json())
+			.then(data => {{
+				for (let i = 0; i < 3; i++) {{
+					document.getElementById("val" + i).innerText = data.brightness[i];
+					document.getElementById("led" + i).value = data.brightness[i];
+				}}
+			}})
+			.catch(err => console.error("Error:", err));
+		}}
+	</script>
+</body>
+</html>
+"""
+
+# --- Request Handler ---
+class LEDHandler(BaseHTTPRequestHandler):
+	def do_GET(self):
+		self.send_response(200)
+		self.send_header("Content-type", "text/html")
+		self.end_headers()
+		self.wfile.write(make_page().encode())
+
+	def do_POST(self):
+		length = int(self.headers['Content-Length'])
+		body = self.rfile.read(length).decode()
+		data = parse_qs(body)
+
+		try:
+			idx = int(data['led'][0])
+			level = int(data['level'][0])
+			brightness[idx] = level
+			pwms[idx].ChangeDutyCycle(level)
+		except:
+			pass
+
+		self.send_response(200)
+		self.send_header("Content-type", "application/json")
+		self.end_headers()
+		self.wfile.write(json.dumps({"brightness": brightness}).encode())
+
+# --- Server setup ---
+def run_server():
+	server = HTTPServer(("", 8080), LEDHandler)
+	print("Server running at http://localhost:8080 (Ctrl+C to stop)")
+	try:
+		server.serve_forever()
+	except KeyboardInterrupt:
+		print("\nStopping server...")
+	finally:
+		server.shutdown()
+		server.server_close()
+		for p in pwms:
+			p.stop()
+		GPIO.cleanup()
+		print("Clean exit complete.")
+
+if __name__ == "__main__":
+	run_server()
