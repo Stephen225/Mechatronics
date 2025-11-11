926f4883c3bcf2e4a265728d3d30b88882ea371b
Stephen225 <128063929+Stephen225@users.noreply.github.com>
2025-11-10 23:02:16 -0500

    Add files via upload

--- lab8.py
+++ lab8.py
@@ -55,8 +55,8 @@ class Stepper:
         else: return(int(abs(x)/x))
 
     # Move a single +/-1 step in the motor sequence:
-    def __step(self, dir):
-        self.step_state += dir    # increment/decrement the step
+    def __step(self, direc):
+        self.step_state += direc   # increment/decrement the step
         self.step_state %= 8      # ensure result stays in [0,7]
         #Stepper.shifter_outputs |= 0b1111<<self.shifter_bit_start
         #Stepper.shifter_outputs &= Stepper.seq[self.step_state]<<self.shifter_bit_start
@@ -66,17 +66,18 @@ class Stepper:
         #print(bin(Stepper.shifter_outputs.value))
         print(str(self.shifter_bit_start)+" "+str(self.step_state), flush=True)
         self.s.shiftByte(Stepper.shifter_outputs.value)
-        self.angle += dir/Stepper.steps_per_degree
+        self.angle += direc/Stepper.steps_per_degree
         self.angle %= 360         # limit to [0,359.9+] range
 
     # Move relative angle from current position:
     def __rotate(self, delta):
         #self.lock.acquire()                 # wait until the lock is available
         numSteps = int(Stepper.steps_per_degree * abs(delta))    # find the right # of steps
-        dir = self.__sgn(delta)        # find the direction (+/-1)
+        direc = self.__sgn(delta)        # find the direction (+/-1)
+        print(numSteps, " ", direc)
         for s in range(numSteps):      # take the steps
             self.lock.acquire()
-            self.__step(dir)
+            self.__step(direc)
             self.lock.release()
             time.sleep(Stepper.delay/1e6)
         #self.lock.release()
