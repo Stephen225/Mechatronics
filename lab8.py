d5391bca47100fb6d3aaf7b36cab31ca2eb8800b
Stephen225 <128063929+Stephen225@users.noreply.github.com>
2025-11-10 22:33:00 -0500

    Add files via upload

--- lab8.py
+++ lab8.py
@@ -56,14 +56,14 @@ class Stepper:
 
     # Move a single +/-1 step in the motor sequence:
     def __step(self, dir):
-        self.step_state += dir    # increment/decrement the step
-        self.step_state %= 8      # ensure result stays in [0,7]
-        #Stepper.shifter_outputs |= 0b1111<<self.shifter_bit_start
-        #Stepper.shifter_outputs &= Stepper.seq[self.step_state]<<self.shifter_bit_start
-        Stepper.shifter_outputs.value &= ~(0b1111<<self.shifter_bit_start)
-        Stepper.shifter_outputs.value |= Stepper.seq[self.step_state]<<self.shifter_bit_start
-        #print(str(self.shifter_bit_start)+" "+str(Stepper.shifter_outputs))
         with self.lock:
+            self.step_state += dir    # increment/decrement the step
+            self.step_state %= 8      # ensure result stays in [0,7]
+            #Stepper.shifter_outputs |= 0b1111<<self.shifter_bit_start
+            #Stepper.shifter_outputs &= Stepper.seq[self.step_state]<<self.shifter_bit_start
+            Stepper.shifter_outputs.value &= ~(0b1111<<self.shifter_bit_start)
+            Stepper.shifter_outputs.value |= Stepper.seq[self.step_state]<<self.shifter_bit_start
+            #print(str(self.shifter_bit_start)+" "+str(Stepper.shifter_outputs))
             print(bin(Stepper.shifter_outputs.value))
             self.s.shiftByte(Stepper.shifter_outputs.value)
         self.angle += dir/Stepper.steps_per_degree
