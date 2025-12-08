cbf1d057bcfc79baaebef6abc473b98df5a5e678
Stephen225 <128063929+Stephen225@users.noreply.github.com>
2025-12-08 14:14:29 -0500

    Add files via upload

--- stepper.py
+++ stepper.py
@@ -78,13 +78,10 @@ class Stepper:
             current = self.angle.value
 
         delta = (angle - current) % 360
-        # this bit causes it to full spin
-        '''
         if delta > 180:
             delta -= 360
         elif delta < -180:
             delta += 360
-            '''
 
         if delta != 0:
             self.queue.put(("goTo", delta))
